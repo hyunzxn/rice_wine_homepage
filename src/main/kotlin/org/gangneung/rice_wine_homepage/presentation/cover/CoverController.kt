aa61c116985bcf86da4920f96092ca10507e721a
hyunzxn <jann1653@gmail.com>
2024-01-27 18:38:02 +0900

    feat: API 요청 응답 시 CommonApiResponse 클래스 추가

--- src/main/kotlin/org/gangneung/rice_wine_homepage/presentation/cover/CoverController.kt
+++ src/main/kotlin/org/gangneung/rice_wine_homepage/presentation/cover/CoverController.kt
@@ -2,7 +2,7 @@ package org.gangneung.rice_wine_homepage.presentation.cover
 
 import org.gangneung.rice_wine_homepage.presentation.cover.request.CoverAppend
 import org.gangneung.rice_wine_homepage.service.cover.CoverService
-import org.springframework.http.ResponseEntity
+import org.gangneung.rice_wine_homepage.util.ApiResponse
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -13,8 +13,8 @@ import org.springframework.web.bind.annotation.RestController
 class CoverController(
     private val coverService: CoverService,
 ) {
-    @PostMapping()
-    fun append(@RequestBody request: CoverAppend): ResponseEntity<Long> {
-        return ResponseEntity.ok().body(coverService.append(request.toCover()))
+    @PostMapping
+    fun append(@RequestBody request: CoverAppend): ApiResponse<Long> {
+        return ApiResponse.ok("소개글 작성이 완료됐습니다", coverService.append(request.toCover()))
     }
 }
