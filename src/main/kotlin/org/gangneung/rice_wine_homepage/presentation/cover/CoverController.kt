88425d01693a6f2324cd4cb5f6c9d047586fc316
hyunzxn <jann1653@gmail.com>
2024-01-29 20:23:54 +0900

    feat: DB에 저장된 소개글(Cover)을 수정할 수 있는 기능 추가

--- src/main/kotlin/org/gangneung/rice_wine_homepage/presentation/cover/CoverController.kt
+++ src/main/kotlin/org/gangneung/rice_wine_homepage/presentation/cover/CoverController.kt
@@ -1,9 +1,12 @@
 package org.gangneung.rice_wine_homepage.presentation.cover
 
 import org.gangneung.rice_wine_homepage.presentation.cover.request.CoverAppend
+import org.gangneung.rice_wine_homepage.presentation.cover.request.CoverModify
 import org.gangneung.rice_wine_homepage.service.cover.CoverService
 import org.gangneung.rice_wine_homepage.util.ApiResponse
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -16,5 +19,10 @@ class CoverController(
     @PostMapping
     fun append(@RequestBody request: CoverAppend): ApiResponse<Long> {
         return ApiResponse.ok("소개글 작성이 완료됐습니다", coverService.append(request.toCover()))
+    }
+
+    @PutMapping("/{id}")
+    fun modify(@RequestBody request: CoverModify, @PathVariable id: Long): ApiResponse<Long> {
+        return ApiResponse.ok("소개글 수정이 완료됐습니다", coverService.modify(id, request))
     }
 }
--- src/main/kotlin/org/gangneung/rice_wine_homepage/service/cover/CoverService.kt
+++ src/main/kotlin/org/gangneung/rice_wine_homepage/service/cover/CoverService.kt
@@ -2,13 +2,24 @@ package org.gangneung.rice_wine_homepage.service.cover
 
 import org.gangneung.rice_wine_homepage.domain.cover.Cover
 import org.gangneung.rice_wine_homepage.implement.cover.CoverAppender
+import org.gangneung.rice_wine_homepage.implement.cover.CoverModifier
+import org.gangneung.rice_wine_homepage.presentation.cover.request.CoverModify
+import org.gangneung.rice_wine_homepage.presentation.cover.request.CoverReader
 import org.springframework.stereotype.Service
 
 @Service
 class CoverService(
+    private val coverReader: CoverReader,
     private val coverAppender: CoverAppender,
+    private val coverModifier: CoverModifier,
 ) {
     fun append(cover: Cover): Long {
         return coverAppender.append(cover)
+    }
+
+    fun modify(id: Long, request: CoverModify): Long {
+        val cover = coverReader.readBy(id)
+        coverModifier.modify(cover, request)
+        return cover.id!!
     }
 }
