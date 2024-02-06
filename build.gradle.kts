c8fba373ef3aacdd1fa92c01bff96f341be41764
hyunzxn <jann1653@gmail.com>
2024-02-06 21:26:06 +0900

    chore: aws s3 사용하기 위한 spring cloud 라이브러리 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,6 +29,7 @@ dependencies {
     // Spring
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.cloud:spring-cloud-starter-aws:2.2.6.RELEASE")
 
     // Kotlin
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
