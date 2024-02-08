2da7d740f191e834e6d6fe8b902071f964f59ec3
hyunzxn <jann1653@gmail.com>
2024-02-08 18:11:46 +0900

    refactor: CoverImageUpload 클래스 삭제

--- src/main/kotlin/org/gangneung/rice_wine_homepage/presentation/coverimage/request/CoverImageUpload.kt
+++
@@ -1,8 +0,0 @@
-package org.gangneung.rice_wine_homepage.presentation.coverimage.request
-
-import org.springframework.web.multipart.MultipartFile
-
-data class CoverImageUpload(
-    val title: String,
-    val imageFile: MultipartFile,
-)
