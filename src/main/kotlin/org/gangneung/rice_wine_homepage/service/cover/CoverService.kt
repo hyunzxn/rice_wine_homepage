923b3c255ff332e371d9c2759d2885719bd9f8f6
hyunzxn <jann1653@gmail.com>
2024-02-08 18:15:23 +0900

    feat: 소개글(Cover) 작성 완료 시 S3에 저장된 파일의 정보를 DB에 저장하는 로직 추가

--- src/main/kotlin/org/gangneung/rice_wine_homepage/service/cover/CoverService.kt
+++ src/main/kotlin/org/gangneung/rice_wine_homepage/service/cover/CoverService.kt
@@ -3,6 +3,7 @@ package org.gangneung.rice_wine_homepage.service.cover
 import org.gangneung.rice_wine_homepage.domain.cover.Cover
 import org.gangneung.rice_wine_homepage.implement.cover.CoverAppender
 import org.gangneung.rice_wine_homepage.implement.cover.CoverModifier
+import org.gangneung.rice_wine_homepage.implement.coverimage.CoverImageAppender
 import org.gangneung.rice_wine_homepage.presentation.cover.request.CoverModify
 import org.gangneung.rice_wine_homepage.presentation.cover.request.CoverReader
 import org.gangneung.rice_wine_homepage.presentation.cover.response.CoverResponse
@@ -13,13 +14,16 @@ class CoverService(
     private val coverReader: CoverReader,
     private val coverAppender: CoverAppender,
     private val coverModifier: CoverModifier,
+    private val coverImageAppender: CoverImageAppender,
 ) {
     fun get(): CoverResponse {
         return CoverResponse.toResponse(coverReader.readBy(1L))
     }
 
     fun append(cover: Cover): Long {
-        return coverAppender.append(cover)
+        val newCover = coverAppender.append(cover)
+        coverImageAppender.append(newCover)
+        return newCover.id!!
     }
 
     fun modify(id: Long, request: CoverModify): Long {
