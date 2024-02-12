0886ecb459256799b4060929040f7e4e41be8231
hyunzxn <jann1653@gmail.com>
2024-02-12 23:02:28 +0900

    feat: Business PK Auto_Increment 조건 추가, Subject 프로퍼티 추가

--- src/main/kotlin/org/gangneung/ricewinehomepage/domain/business/Business.kt
+++ src/main/kotlin/org/gangneung/ricewinehomepage/domain/business/Business.kt
@@ -2,6 +2,8 @@ package org.gangneung.ricewinehomepage.domain.business
 
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.Lob
 
@@ -16,7 +18,10 @@ class Business private constructor(
     @Lob
     @Column(nullable = false)
     val content: String,
+    @Column(nullable = false)
+    val subject: String,
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null,
 ) {
     companion object {
@@ -24,12 +29,14 @@ class Business private constructor(
             name: String,
             contact: String,
             email: String,
+            subject: String,
             content: String,
         ): Business {
             return Business(
                 name = name,
                 contact = contact,
                 email = email,
+                subject = subject,
                 content = content,
             )
         }
