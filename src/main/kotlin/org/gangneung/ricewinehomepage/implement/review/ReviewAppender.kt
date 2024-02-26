c24a5e876df160d5637d3f33f0c63e7728923603
hyunzxn <jann1653@gmail.com>
2024-02-26 14:50:51 +0900

    refactor: ReviewRepository 패키지 위치 변경

--- src/main/kotlin/org/gangneung/ricewinehomepage/implement/review/ReviewAppender.kt
+++ src/main/kotlin/org/gangneung/ricewinehomepage/implement/review/ReviewAppender.kt
@@ -1,7 +1,7 @@
 package org.gangneung.ricewinehomepage.implement.review
 
 import org.gangneung.ricewinehomepage.domain.review.Review
-import org.gangneung.ricewinehomepage.repository.ReviewRepository
+import org.gangneung.ricewinehomepage.repository.review.ReviewRepository
 import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
 
