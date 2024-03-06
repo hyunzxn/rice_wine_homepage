0459e7812173996ab96b12b1d8e87669e83f3d62
hyunzxn <jann1653@gmail.com>
2024-03-06 16:41:02 +0900

    feat: OAuth2 카카오 로그인 기능 추가

--- src/main/kotlin/org/gangneung/ricewinehomepage/util/security/oauth2/SocialRegistration.kt
+++ src/main/kotlin/org/gangneung/ricewinehomepage/util/security/oauth2/SocialRegistration.kt
@@ -3,6 +3,7 @@ package org.gangneung.ricewinehomepage.util.security.oauth2
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.security.oauth2.client.registration.ClientRegistration
 import org.springframework.security.oauth2.core.AuthorizationGrantType
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod.CLIENT_SECRET_POST
 import org.springframework.security.oauth2.core.oidc.IdTokenClaimNames
 import org.springframework.stereotype.Component
 
@@ -19,6 +20,12 @@ class SocialRegistration {
 
     @Value("\${oauth.google.client-secret}")
     private lateinit var googleClientSecret: String
+
+    @Value("\${oauth.kakao.client-id}")
+    private lateinit var kakaoClientId: String
+
+    @Value("\${oauth.kakao.client-secret}")
+    private lateinit var kakaoClientSecret: String
 
     fun naverClientRegistration(): ClientRegistration {
         return ClientRegistration.withRegistrationId("naver")
@@ -47,6 +54,21 @@ class SocialRegistration {
             .issuerUri("https://accounts.google.com")
             .userInfoUri("https://www.googleapis.com/oauth2/v3/userinfo")
             .userNameAttributeName(IdTokenClaimNames.SUB)
+            .build()
+    }
+
+    fun kakaoClientRegistration(): ClientRegistration {
+        return ClientRegistration.withRegistrationId("kakao")
+            .clientId(kakaoClientId)
+            .clientSecret(kakaoClientSecret)
+            .clientAuthenticationMethod(CLIENT_SECRET_POST)
+            .redirectUri("http://localhost:8080/login/oauth2/code/kakao")
+            .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+            .scope("profile_nickname", "account_email")
+            .authorizationUri("https://kauth.kakao.com/oauth/authorize")
+            .tokenUri("https://kauth.kakao.com/oauth/token")
+            .userInfoUri("https://kapi.kakao.com/v2/user/me")
+            .userNameAttributeName("id")
             .build()
     }
 }
